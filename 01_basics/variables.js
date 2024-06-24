30386f5a6a535b619990657e2a120082f729d76c
Mohaamad Kasim <kasimwasi111@gmail.com>
2024-06-24 11:04:44 +0530

    done with variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 8455;
+let accountEmail = "kasim111wasi@yahoo.com";
+var accountPwd = "7894";
+accountCity = "Ghosi";
+let accountState; //undefined
+
+// accountId = 2; // Not allowed
+
+accountEmail = "kasim@12.yahoo";
+accountPwd = "5481";
+accountCity = "mau";
+
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState]);
+accountState = "UP";
+accountPwd = "1254";
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState]);

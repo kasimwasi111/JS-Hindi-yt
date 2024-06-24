d05ae55bcee412b70e25f9b6d71a75d15335ab5f
Mohaamad Kasim <kasimwasi111@gmail.com>
2024-06-24 11:44:51 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3); // we are using nodejs, not browser
+
+// Primitive Datatypes
+
+/*
+number
+bigInt
+string
+boolean
+null => standalone value
+undefined
+symbol => unique
+*/
+
+// Non-primitive
+
+/*
+Object
+*/
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined

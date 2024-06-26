e3827ddf279a97a0c3bafc2ab5f26125aca73c73
Mohaamad Kasim <kasimwasi111@gmail.com>
2024-06-26 15:03:57 +0530

    Conversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,32 @@ console.log(booleanIsLoggedIn); //true
 // "kasim"=>true
 // 7=>true
 // -7=>true
+
+// ****************************Operations*******************
+
+let value = 4;
+let negValue = -value;
+console.log(negValue); //-4
+
+console.log(2 + 2); //4
+console.log(2 - 2); //0
+console.log(2 * 2); //4
+console.log(2 ** 3); //8
+console.log(2 / 3); //0.66666
+console.log(2 % 3); //2
+
+let str1 = "Hello";
+let str2 = " kasim";
+console.log(str1 + str2); //Hello kasim
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+console.log(3 + ((4 * 5) % 3)); //5
+console.log(+true); //1
+console.log(+""); //0
+
+let counter = 100;
+counter++;
+console.log(counter); //101
+console.log(++counter); //102
